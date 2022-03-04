e424135f5ac4eba2b6c166b79b6ecc3d94d74659
hnurlan7 <tu6f7xcr3@code.edu.az>
2022-03-04 21:01:44 +0400

    add task2 file

---
+++ task-2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //int Num = Convert.ToInt32(Console.ReadLine());
+            //int a = 0;
+            //if (Num != 0)
+            //{
+            //    while (Num > 0)
+            //    {
+            //        Num /= 10;
+            //        a++;
+            //    }
+            //    if (Num < 1)
+            //    {
+            //        Console.WriteLine(a);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Enter positive numbers!");
+            //}
+            #endregion
+
+            #region task2
+            string txt = "Nurlan";
+            int count = 0;
+            for (int i = 0; i < txt.Length; i++)
+            {
+                if (txt[i] == 'a')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            #endregion
+
+            #region task3
+
+            //int[] arr = { 4, 7, 9, 14, 15, 19, 21 };
+            //int sum = 0;
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] % 3 == 0)
+            //    {
+            //        sum += arr[i];
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+            #endregion
+        }
+    }
+}
